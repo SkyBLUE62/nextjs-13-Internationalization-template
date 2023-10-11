5d906b1e720cc29f488ac83a71d8ca7e54fae8d4
Thomas <totoalexandre6259@gmail.com>
2023-10-11 17:13:13 +0200

    Lang folder

---
+++ src/app/[lang]/page.jsx
@@ -0,0 +1,9 @@
+"use server";
+
+import { getDictionary } from "./dictionaries";
+
+export default async function Page({ params: { lang } }) {
+  const dict = await getDictionary(lang); // en
+
+  return <button>{dict.products.cart}</button>; // Add to Cart
+}
